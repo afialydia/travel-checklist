0d412260affdf43ec7a60e82493d1c99e8226e80
afialydia <afia.caruso@gmail.com>
2022-05-29 12:20:17 -0400

    styled app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import "./App.css";
 import Checklist from "./components/checklist";
 import NewItemForm from "./components/newItemForm";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Travel Checklist</h1>
-			<NewItemForm />
-			<Checklist />
+		<div className="App w-screen h-screen bg-gray-200 overflow-hidden select-none text-center">
+			<div className="w-5/6 mx-auto pt-5 md:pt-20 h-full">
+				<h1 className="text-3xl font-bold underline mb-4">Travel Checklist</h1>
+				<div className="flex flex-col md:flex-row justify-evenly">
+					<NewItemForm />
+					<Checklist />
+				</div>
+			</div>
 		</div>
 	);
 }
