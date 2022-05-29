1e4bf43ae267e99b6710167454cb5fa8d3680c6c
afialydia <afia.caruso@gmail.com>
2022-05-29 12:19:23 -0400

    tailwind setup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-//libraries
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
-//styles
-
-import "./index.css";
-
-//files
 import App from "./App.js";
 import { store, persistor } from "./redux/store.js";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-	
-		<Provider store={store}>
-				<PersistGate persistor={persistor}>
-					<App />
-				</PersistGate>
-		</Provider>
+	<Provider store={store}>
+		<PersistGate persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>
 );
